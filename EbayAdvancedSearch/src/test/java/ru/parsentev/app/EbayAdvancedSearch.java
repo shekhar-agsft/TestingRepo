84566e2412e8c6bfe8956c10ff9cea6e251a5fef
shekhar-agsft <shekharc2229@gmail.com>
2017-08-11 11:20:41 +0530

    testing

---
+++ EbayAdvancedSearch/src/test/java/ru/parsentev/app/EbayAdvancedSearch.java
@@ -0,0 +1,40 @@
+package ru.parsentev.app;
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;	
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;		
+public class EbayAdvancedSearch {			
+	 WebDriver driver = new HtmlUnitDriver();
+	@Test
+	public void setUp() throws Exception {
+		  driver.get("http://www.google.com");			
+		  // Locate the searchbox using its name		
+          WebElement element = driver.findElement(By.name("q"));	
+          
+         // Enter a search query		
+         element.sendKeys("Guru99");	
+        
+         // Submit the query. Webdriver searches for the form using the text input element automatically		
+         // No need to locate/find the submit button		
+         element.submit();			
+         
+ 		// This code will print the page title		
+         System.out.println("Page title is: " + driver.getTitle());		
+         
+         driver.quit();		
+		//selenium = new WebDriverBackedSelenium(driver, baseUrl);
+	}
+	
+		
+}
